28b7a22c59abeec3a61b565aa9d7f1b1323e6e81
Peter Darche <pdarche@gmail.com>
2018-05-01 19:25:02 -0400

    Change requests to https

--- app/scripts/services/project.js
+++ app/scripts/services/project.js
@@ -10,7 +10,7 @@
 
 angular.module('darcheApp')
   .factory('Project', ['$resource', function($resource) {
-    return $resource('http://peterdarche.com:5984/projects/:id', {id: '@_id'}, {
+    return $resource('https://peterdarche.com:5984/projects/:id', {id: '@_id'}, {
       save: {
         method: 'POST',
         withCredentials: true
@@ -22,7 +22,7 @@ angular.module('darcheApp')
       query: {
         method: 'GET',
         params: {include_docs: true},
-        url: 'http://peterdarche.com:5984/projects/_all_docs',
+        url: 'https://peterdarche.com:5984/projects/_all_docs',
         headers: {'Content-Type':'text/plain'},
         withCredentials: true,
         interceptor: {
