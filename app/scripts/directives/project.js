2525de1522b4fefe8923224e357cff82dea1f9ba
Peter Darche <pdarche@gmail.com>
2018-05-24 16:21:50 -0400

    Move to jquery slim

--- app/scripts/directives/project.js
+++ app/scripts/directives/project.js
@@ -16,32 +16,10 @@ angular
       restrict: 'AEC',
       templateUrl: 'views/projectContent.html',
       link: function(scope, el, attrs) {
-        var video = el.find('video').first();
-        var source = el.find('video source').first();
-
-        // Unhide the content
-        el.removeClass('ng-hide');
-
         // Typeset any math
         $timeout(function() {
           MathJax.Hub.Queue(["Typeset", MathJax.Hub])
         }, 2000)
-
-        // Set the video attributes
-        video.attr('poster', scope.project.imageUrl);
-        video.hide().on('loadeddata', function(){
-          video.fadeIn('slow');
-        })
-        source.attr('src', scope.project.gfycat);
-
-        // Bind the events
-        el.on('mouseover', '.project-partial__image', function(ev){
-          ev.target.play();
-        });
-
-        el.on('mouseout', '.project-partial__image', function(ev){
-          ev.target.pause();
-        });
 
         el.on('click', '.delete', function(ev){
           ev.preventDefault();
